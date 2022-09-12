c91b5f3f6cb1a4992733c0eb7a806e70afa8060d
Anton Kraus <students@gci-kw.de>
2022-09-12 15:50:15 +0200

    WIP categories correct "checked" state

--- pg_metadata/edit_layer_metadata.py
+++ pg_metadata/edit_layer_metadata.py
@@ -6,6 +6,7 @@ Created on Thu Sep  8 14:16:32 2022
 """
 
 import logging
+from collections import OrderedDict
 
 from qgis.PyQt.QtWidgets import QDialog
 from qgis.core import QgsProviderConnectionException
@@ -17,10 +18,21 @@ LOGGER = logging.getLogger('pg_metadata')
 EDITDIALOG_CLASS = load_ui('edit_metadata_dialog.ui')
 
 
-
 def postgres_array_to_list(s: str) -> list[str]:
     return s[1:-1].split(',')
 
+
+def get_glossary(connection, field: str) -> OrderedDict:
+    sql = f"SELECT code, label_de FROM pgmetadata.glossary WHERE field = '{field}' ORDER BY item_order"
+    try:
+        rows = connection.executeSql(sql)
+    except QgsProviderConnectionException as e:
+        LOGGER.critical(tr('Error when querying the database: ') + str(e))
+        return False
+    terms = OrderedDict()
+    for row in rows:
+        terms[row[0]] = row[1]
+    return terms
 
 
 class PgMetadataLayerEditor(QDialog, EDITDIALOG_CLASS):
@@ -30,10 +42,9 @@ class PgMetadataLayerEditor(QDialog, EDITDIALOG_CLASS):
         self.setupUi(self)
 
     def open_editor(self, datasource_uri, connection):
-        #self.show()
         table = datasource_uri.table()
         schema = datasource_uri.schema()
-        LOGGER.critical(f'Edit layer type {datasource_uri.table()}, {connection}')
+        LOGGER.info(f'Edit layer type {datasource_uri.table()}, {connection}')
         sql = (f"SELECT title, abstract, project_number, categories, keywords, themes FROM pgmetadata.dataset "
                f"WHERE schema_name = '{schema}' and table_name = '{table}'")
         try:
@@ -48,10 +59,14 @@ class PgMetadataLayerEditor(QDialog, EDITDIALOG_CLASS):
         self.textbox_keywords.setPlainText(str(data[0][4]))
         #self.textbox_themes.setPlainText(data[0][5])
         
-        categories = postgres_array_to_list(data[0][3])
-        
-        LOGGER.critical(tr(f'Kategorien: {type(data[0][3])}'))
-        self.comboBox_categories.addItems(categories)
+        # get categories and fill comboBox
+        categories = get_glossary(connection, 'dataset.categories')
+        selected_categories_keys = postgres_array_to_list(data[0][3])
+        selected_categories_values = []
+        for i in selected_categories_keys:
+            selected_categories_values.append(categories[i])
+        self.comboBox_categories.addItems(categories.values())  # fill comboBox with categories
+        self.comboBox_categories.setCheckedItems(selected_categories_values)  # set selected categories as checked
         
         self.show()
         result = self.exec_()
