59a9ce8763028fc89545b7a28dffa8373789650e
Florian Jenn <florian.jenn@gci-kw.de>
2022-12-07 16:48:58 +0100

    Bugfix: keep local sql for install and upgrade in sync

--- pg_metadata/install/sql/upgrade/local.sql
+++ pg_metadata/install/sql/upgrade/local.sql
@@ -19,9 +19,12 @@ INSERT INTO pgmetadata.glossary (field, code, label_en, description_en, item_ord
 
 -- additional contact roles
 INSERT INTO pgmetadata.glossary (field, code, label_en, description_en, item_order, label_fr, description_fr, label_it, description_it, label_es, description_es, label_de, description_de) VALUES ('contact.contact_role', 'WA', 'WMS/WFS Administrator', 'Person or party who can aid with WMS/WFS issues', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'WMS/WFS-Ansprechpartner', 'Person oder Stelle, die bei WMS/WFS-Problemen weiterhelfen kann') ON CONFLICT DO NOTHING;
-INSERT INTO pgmetadata.glossary (field, code, label_en, description_en, item_order, label_fr, description_fr, label_it, description_it, label_es, description_es, label_de, description_de) VALUES ('contact.contact_role', 'GA', 'GIS Administrator', 'Person or party who can aid with GIS-related issues', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'GIS-technischer Ansprechpartner', 'Person oder Stelle, die in allen GIS-technischen Angelegenheiten weiterhelfen kann') ON CONFLICT DO NOTHING;
+INSERT INTO pgmetadata.glossary (field, code, label_en, description_en, item_order, label_fr, description_fr, label_it, description_it, label_es, description_es, label_de, description_de) VALUES ('contact.contact_role', 'GA', 'GIS Administrator', 'Person or party who can aid with GIS-related issues', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'GIS-technischer Ansprechpartner', 'Person oder Stelle, die bei GIS-technischen Angelegenheiten weiterhelfen kann') ON CONFLICT DO NOTHING;
 
-SELECT pg_catalog.setval('pgmetadata.glossary_id_seq', 149, true);
+-- localisation for left-hand side of scale fractions
+INSERT INTO pgmetadata.glossary (field, code, label_en, description_en, item_order, label_fr, description_fr, label_it, description_it, label_es, description_es, label_de, description_de) VALUES ('display_settings', 'scale_fraction', '1 : ', NULL, NULL, '1/', NULL, NULL, NULL, NULL, NULL, '1 : ', NULL) ON CONFLICT DO NOTHING;
+
+SELECT pg_catalog.setval('pgmetadata.glossary_id_seq', 150, true);
 
 
 -- new item_order for existing publication frequencies
@@ -108,8 +111,8 @@ CREATE OR REPLACE VIEW pgmetadata.v_dataset AS
             gtheme.label AS theme,
             s.keywords,
             s.spatial_level,
-            ('1 : '::text || s.minimum_optimal_scale) AS minimum_optimal_scale,
-            ('1 : '::text || s.maximum_optimal_scale) AS maximum_optimal_scale,
+            (((((glossary.dict -> 'display_settings'::text) -> 'scale_fraction'::text) -> 'label'::text) ->> glossary.locale) || s.minimum_optimal_scale) AS minimum_optimal_scale,
+            (((((glossary.dict -> 'display_settings'::text) -> 'scale_fraction'::text) -> 'label'::text) ->> glossary.locale) || s.maximum_optimal_scale) AS maximum_optimal_scale,
             s.publication_date,
             ((((glossary.dict -> 'dataset.publication_frequency'::text) -> s.publication_frequency) -> 'label'::text) ->> glossary.locale) AS publication_frequency,
             ((((glossary.dict -> 'dataset.license'::text) -> s.license) -> 'label'::text) ->> glossary.locale) AS license,
@@ -161,19 +164,6 @@ CREATE OR REPLACE VIEW pgmetadata.v_dataset AS
 
 -- VIEW v_dataset
 COMMENT ON VIEW pgmetadata.v_dataset IS 'Formatted version of dataset data, with all the codes replaced by corresponding labels taken from pgmetadata.glossary. Used in the function in charge of building the HTML metadata content.';
-
-
--- translated glossary for attribute forms in admin project
-create or replace view pgmetadata.v_glossary_translation_de as
-select id, field, code, item_order, coalesce(label_de, label_en) as label, coalesce(description_de, description_en) as description
-from pgmetadata.glossary order by field, code, item_order;
-
-create or replace view pgmetadata.v_glossary_translation_en as
-select id, field, code, item_order, label_en as label, description_en as description
-from pgmetadata.glossary order by field, code, item_order;
-
-comment on view pgmetadata.v_glossary_translation_de is 'Translation of glossary labels and descriptions, with fallback to English if terms are not translated';
-
 
 
 COMMIT;
